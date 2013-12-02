baa05ddc2d05e1f5415a553731ad85f28f69abe1
Forrest <forrest.shi@gmail.com>
2013-12-02 17:18:02 +0800

    basic effect done

--- WaterDrop/ViewController.m
+++ WaterDrop/ViewController.m
@@ -7,8 +7,12 @@
 //
 
 #import "ViewController.h"
+@import CoreGraphics;
+#import <QuartzCore/QuartzCore.h>
 
-@interface ViewController ()
+@interface ViewController (){
+    UILabel *circle;
+}
 
 @end
 
@@ -18,7 +22,46 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    
+    circle = [[UILabel alloc] initWithFrame:CGRectMake(0, 0, 40, 40)];
+    circle.layer.cornerRadius = circle.bounds.size.width/2.;
+    circle.center = self.view.center;
+    circle.backgroundColor = [UIColor blueColor];
+    
+    [self.view addSubview:circle];
+    
+    UITapGestureRecognizer *tap = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(onTap:)];
+    [self.view addGestureRecognizer:tap];
+    
 }
+
+- (void)onTap:(UITapGestureRecognizer*)gesture{
+    
+    //UIView *tapedView = [gesture.view copy];
+    UILabel *tapedView = [[UILabel alloc] initWithFrame:circle.frame];
+    tapedView.backgroundColor = [UIColor blueColor];
+    tapedView.layer.cornerRadius = tapedView.bounds.size.width/2.;
+    tapedView.center = circle.center;
+
+    [self.view insertSubview:tapedView belowSubview:circle];
+    
+    [UIView animateKeyframesWithDuration:.5 delay:0. options:UIViewKeyframeAnimationOptionCalculationModeCubic animations:^{
+        //
+        tapedView.transform = CGAffineTransformScale(tapedView.transform, 10., 10.);
+        tapedView.alpha = 0.;
+        
+        circle.transform = CGAffineTransformScale(circle.transform, 1.5, 1.5);
+    } completion:^(BOOL finished) {
+        //
+        [UIView animateKeyframesWithDuration:.5 delay:0. options:UIViewKeyframeAnimationOptionCalculationModeCubicPaced animations:^{
+            circle.transform = CGAffineTransformIdentity;
+        } completion:^(BOOL finished) {
+            
+        }];
+    }];
+
+}
+
 
 - (void)didReceiveMemoryWarning
 {
