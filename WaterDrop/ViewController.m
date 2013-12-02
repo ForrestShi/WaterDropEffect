2ae9ba56043d65edd1a82cc58979c5dee3769a7b
Forrest <forrest.shi@gmail.com>
2013-12-02 16:49:25 +0800

    Initial Commit

---
+++ WaterDrop/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  WaterDrop
+//
+//  Created by Shi Lin on 12/2/13.
+//  Copyright (c) 2013 Forrest Shi. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
